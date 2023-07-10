87f9daeb4d131c813a2a89bcd394e2c3341efdf3
exosolarplanet <ecedenniz@gmail.com>
2023-07-10 21:02:58 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# movies-team-two
+# Movie Finder and Recommendation App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+A Flask based API backend was added in the *api* directory.
+
+Read the main [tutorial](https://blog.miguelgrinberg.com/post/how-to-create-a-react--flask-project) on how to create Flask + React combined projects.
+
+## Start Flask API
+```
+source api/venv/bin/activate
+pip install -r api/requirements.txt
+yarn start-api
+```
+
+## Start React App
+Start a new terminal and run:
+```
+yarn start
+```
+
+## Before running Jupyter Notebook
+Start a new terminal and run:
+```
+pip install -r api/requirements.txt
+```
---
+++ api/api.py
@@ -0,0 +1,24 @@
+import requests
+from flask import Flask
+
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+@app.route('/movies/<searchValue>')
+def get_movies(searchValue):
+    url = "http://www.omdbapi.com/?s=" + str(searchValue) + "&apikey=2651b0db"
+    response = requests.get(url=url)
+    return response.json()
+
+@app.route('/movies/imdbID/<searchValue>')
+def get_movies_by_id(searchValue):
+    url = "http://www.omdbapi.com/?i=" + str(searchValue) + "&apikey=2651b0db"
+    response = requests.get(url=url)
+    return response.json()
